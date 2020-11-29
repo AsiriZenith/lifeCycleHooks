7ab2f63426a5be29d2fb45c418ab57275531d505
Asiri Senith <asirisenith@gmail.com>
2020-11-29 21:31:45 +0530

    implemented angular lifecycle hooks

--- lifeCycleHooks/src/app/app.component.ts
+++ lifeCycleHooks/src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'lifeCycleHooks';
+
+  private number: number = 24556;
+  name: string = "Asiri";
+
+  user = {
+    name: "Senith"
+  }
+
+  updateValue() {
+    this.name = "Manjitha";
+    //this.user.name="Aunty";
+  }
+
+  get counter() {
+    return this.number;
+  }
+
+  set counter(_value) {
+    this.number = _value;
+  }
+
+  increment() {
+    this.counter++;
+  }
+
+  decrement() {
+    this.counter--;
+  }
+
 }
--- lifeCycleHooks/src/app/components/child/child.component.ts
+++ lifeCycleHooks/src/app/components/child/child.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChange, SimpleChanges, DoCheck } from '@angular/core';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.scss']
 })
-export class ChildComponent implements OnInit {
+export class ChildComponent implements OnInit, OnChanges, DoCheck {
+
+  private myNumber: number;
+  @Input() myString: string;
 
   constructor() { }
 
+  ngDoCheck(): void {
+
+  }
+
+  @Input()
+  set myNewNumber(number: number) {
+    this.myNumber = number;
+  }
+
+  get myNewNumber() {
+    return this.myNumber;
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    const newNumberChange: SimpleChange = changes.myNewNumber;
+    console.log("Previous Value-(ng On change)", newNumberChange.previousValue)
+    console.log("Current Value-(ng On change)", newNumberChange.currentValue)
+    this.myNewNumber = newNumberChange.currentValue
+  }
+
   ngOnInit(): void {
+    console.log("Current Value-(ng OnInit)", this.myNumber)
   }
 
 }
